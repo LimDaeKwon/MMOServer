61d3b1dae5f65495e21685618b790d7783be2ea3
DK <gritdrake@gmail.com>
2026-05-28 16:25:12 +0900

    #1 SingleChatServer 프로젝트 추가

---
+++ Servers/SingleChatServer/ChattingServer_Single.h
@@ -0,0 +1,141 @@
+#pragma once
+
+//#include "LanNetworkLibraryServerForEchoTest.h"
+#include "NetLibrary.h"
+#include "LockFreeQueue.h"
+#include "TLSObjectFreeList.h"
+#include "CommonProtocol.h"
+#include "ObjectFreeList.h"
+#include <unordered_map>
+#include <list>
+
+//class ContentsCPacket;
+
+
+class ChattingServerSingle : public NetLibrary
+{
+public:
+	enum Type
+	{
+		ACCEPT,
+		MESSAGE,
+		RELEASE,
+	};
+
+	struct Player
+	{
+		INT64 SessionID;
+		INT64 LastRecvTime;//하트비트용
+		INT64 AccountNo;
+
+		WORD SectorX;
+		WORD SectorY;
+
+		WCHAR ID[20];
+		WCHAR NickName[20];
+
+		bool AuthFlag; //인증되지 않은 유저로부터 온 메시지는 결함이니까.
+		bool Duplicate;
+	};
+
+	ObjectFreeList<Player> PlayerPool;
+	std::unordered_map<__int64, Player*> PlayerMap;
+	std::unordered_map<__int64, Player*> AccountMap;
+
+
+	struct SectorPos
+	{
+		unsigned int X;
+		unsigned int Y;
+
+	};
+
+	struct SectorAround
+	{
+		unsigned int Count;
+		SectorPos Around[9];
+	};
+
+	std::list<Player*> SectorList[MAXSECTORY][MAXSECTORX];
+
+	struct MessageData
+	{
+		int type;
+		__int64 session_ID;
+		ContentsCPacket* contents_packet;
+	};
+
+
+	LFQ<MessageData*> MessageQueue;
+	TLSObjectFreeList<MessageData> MessageDataFreeList;
+	HANDLE MessageEvent;
+
+
+
+	ChattingServerSingle();
+	virtual ~ChattingServerSingle();
+
+	virtual bool OnConnectionRequest(const wchar_t* server_IP, unsigned short server_port);
+	virtual void OnAccept(const wchar_t* server_IP, unsigned short server_port, __int64 session_ID);
+	virtual void OnRelease(__int64 session_ID);
+	virtual void OnMessage(__int64 session_ID, ContentsCPacket* send_packet);
+	virtual void OnError(int errorcode, const wchar_t* error_log);
+	virtual void OnInitializeTPS();
+
+	void AcceptProc(MessageData* msg_data);
+	void MessageProc(MessageData* msg_data);
+	void ReleaseProc(MessageData* msg_data);
+
+
+	void NetPacketProc_Login(Player* target, INT64 AccountNo,WCHAR* ID, WCHAR* NickName, char* SessionKey);
+	void NetPacketProc_SectorMove(Player* target, INT64 AccountNo, WORD SectorX, WORD SectorY);
+	void NetPacketProc_Message(Player* target, INT64 AccountNo,WORD MessageLen, WCHAR* Message);
+	void NetPacketProc_Heartbeat(Player* target);
+
+	void GetSectorAround(int SectorX, int SectorY, SectorAround* AroundSector);
+
+	DWORD LogicTPS;
+	DWORD LoginTPS;
+	DWORD SectorMoveTPS;
+	DWORD ChatTPS;
+	DWORD HeartBeatTPS;
+
+	DWORD LogicCount;
+	DWORD LoginCount;
+	DWORD SectorMoveCount;
+	DWORD ChatCount;
+	DWORD HeartBeatCount;
+
+	DWORD DCWrongPacket;
+	DWORD DCAuthFailed;
+	DWORD DCDuplicateLogin;
+
+	DWORD LoginPlayer;
+	DWORD UnloginPlayer;
+
+
+	
+	DWORD GetLogicTPS();
+	DWORD GetLoginTPS();
+	DWORD GetSectorMoveTPS();
+	DWORD GetChatTPS();
+	DWORD GetHeartBeatTPS();
+
+	DWORD GetDCWrongPacket();
+	DWORD GetDCAuthFailed();
+	DWORD GetDCDuplicateLogin();
+
+	DWORD GetLoginPlayer();
+	DWORD GetUnloginPlayer();
+
+	DWORD GetLogicQueueSize();
+	
+
+	HANDLE LogicThreadHandle;
+	static unsigned int WINAPI LogicThread(LPVOID this_ptr);
+
+
+
+
+
+};
---
+++ Servers/SingleChatServer/SingleChatMain.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include "ChattingServer_Single.h"
+#include "windows.h"
+#include "conio.h"
+#include "Profiler.h"
+#include "CrashDump.h"
+#include "SystemMonitoring.h"
+#include "ProcessMonitoring.h"
+
+
+CrashDump zz;
+
+#pragma comment(lib, "winmm.lib")
+
+#define FILENAME "SingleChatThreadSetting.config"
+
+
+enum thread_setting_enum // 여기에 SendBuffer 사이즈 설정 옵션도 넣기. 
+{
+	IP, PORT, THREADS, CONCURRENT, NAGLE, SESSIONS, HEADERSIZE
+};
+unsigned int GlobalChecksum;
+
+#define RFLAG 0x80000000
+
+bool ParseThreadDataFile(const char* file_name);
+
+char ThreadData[7][200];
+
+
+int main()
+{
+
+	SystemMonitoring a;
+	ProcessMonitoring b;
+
+	timeBeginPeriod(1);
+	InitProfile();
+	ParseThreadDataFile(FILENAME);
+
+	//ChattingServerSingle* ChatInstance = new ChattingServerSingle;
+	//ChatInstance->Start(ThreadData[IP], atoi(ThreadData[PORT]), atoi(ThreadData[THREADS]), atoi(ThreadData[CONCURRENT]), atoi(ThreadData[NAGLE]), atoi(ThreadData[SESSIONS]), atoi(ThreadData[HEADERSIZE]));
+
+
+	ChattingServerSingle* ChatInstance = new ChattingServerSingle;
+	ChatInstance->Start(ThreadData[IP], atoi(ThreadData[PORT]), atoi(ThreadData[THREADS]), atoi(ThreadData[CONCURRENT]), atoi(ThreadData[NAGLE]), atoi(ThreadData[SESSIONS]), atoi(ThreadData[HEADERSIZE]));
+
+
+
+	while (1)
+	{
+		wprintf(L"\n------------------------------------------------------------ \n");
+
+		wprintf(L"CPakcet \n");
+		wprintf(L"UseSize  : %d   Capacity  :  %d\n", CPacket::GetUseSize(), CPacket::GetCapacity());
+		wprintf(L"LogicQ  :  %d \n", ChatInstance->GetLogicQueueSize());
+		wprintf(L"Contents \n");
+		wprintf(L"Session  :  %d  \n", ChatInstance->GetSessionNum());
+		wprintf(L"UnloginPlayer  :  %d  Player  :  %d   \n", ChatInstance->GetUnloginPlayer(), ChatInstance->GetLoginPlayer());
+
+
+		wprintf(L"\n------------------------------------------------------------ \n");
+		wprintf(L"Disconnect \n");
+		wprintf(L"DisconnectTotal  :  %d \n", ChatInstance->GetDisconnectCount());
+		wprintf(L"DCWrongPacket :  %d	    DCAuthFailed  :  %d \n", ChatInstance->GetDCWrongPacket(), ChatInstance->GetDCAuthFailed());
+		wprintf(L"DCUnloginTimeout  :  %d     DCLoginTimeout  :  %d \n", ChatInstance->GetDCUnloginTimeout(), ChatInstance->GetDCLoginTimeout());
+		wprintf(L"DCSendBufferFull  :  %d     DCDuplicateLogin  :  %d\n", ChatInstance->GetDCSendBufferFull(), ChatInstance->GetDCDuplicateLogin());
+		wprintf(L"DCPacketCodeError  :  %d   DCSessionFull  :  %d \n", ChatInstance->GetDCPacketCodeError(), ChatInstance->GetDCSessionFull());
+		wprintf(L"DCDecodeError  :  %d  DCImpossiblePacketLength  :  %d \n", ChatInstance->GetDCDecodeError(), ChatInstance->GetDCImpossiblePacketLength());
+
+		wprintf(L"\n------------------------------------------------------------ \n");
+		wprintf(L"TPS \n");
+		wprintf(L"AccpetTPS  :  %d \n", ChatInstance->GetAcceptTPS());
+		wprintf(L"RecvTPS  :  %d     SendTPS  :  %d \n", ChatInstance->GetRecvMessageTPS(), ChatInstance->GetSendMessageTPS());
+		wprintf(L"LogicTPS  :  %d      LoginTPS  :  %d\n", ChatInstance->GetLogicTPS(), ChatInstance->GetLoginTPS());
+		wprintf(L"SectorMoveTPS  :  %d     ChatTPS  :  %d \n", ChatInstance->GetSectorMoveTPS(), ChatInstance->GetChatTPS());
+
+		wprintf(L"\n------------------------------------------------------------ \n");
+		wprintf(L"Monitoring \n\n");
+		wprintf(L"ProcessNonPagedPool  : %f%%   ServerNonPagedPool  :  %f%% \n", b.GetProcessNPMemory(), a.GetServerNonPagedBytes());
+		wprintf(L"ProcessUseMemory: %f%%  ServerAvailableMemory: %f%%  \n", b.GetProcessUserMemory(), a.GetServerAvailableMBytes());
+		wprintf(L"ProcessCPUUsage: %f%%   ServerCPUUsage: %f%%\n", b.ProcessTotal(), a.ProcessorTotal());
+
+
+
+
+
+		Sleep(1000);
+
+
+		if (_kbhit())
+		{
+			char c = _getch();
+			if (c == 's' || c == 'C')
+			{
+				ProfileDataOutText(L"EchoProfileData");
+			}
+
+			if (c == 'r' || c == 'R')
+			{
+				ProfileReset();
+			}
+		}
+	}
+
+
+	timeEndPeriod(1);
+
+}
+
+
+bool ParseThreadDataFile(const char* file_name)
+{
+	FILE* worker_information;
+	int file_size;
+	char* file_buffer;
+
+	if (fopen_s(&worker_information, file_name, "rb") == 0)
+	{
+		if (worker_information == NULL)
+		{
+			return false;
+		}
+
+
+		fseek(worker_information, 0, SEEK_END);
+		file_size = ftell(worker_information);
+		rewind(worker_information);
+
+
+		file_buffer = (char*)malloc(file_size);
+		if (file_buffer == NULL) 
+		{
+			printf("Error: Memory allocation failed.\n");
+			DebugBreak();
+			return false;
+		}
+
+		size_t bytesRead = fread_s(file_buffer, file_size, 1, file_size, worker_information);
+		if (bytesRead != file_size)
+		{
+
+			printf("Error: Failed to read entire file. Expected %d bytes, Read: %zu bytes.\n", file_size, bytesRead);
+
+			return false;
+		}
+
+		int start_position = 0;
+		int end_postiion = 0;
+
+		//시작위치 + 0d 0a 넘기기. 
+
+		for (int i = 0; i < file_size; ++i)
+		{
+			if (file_buffer[i] == '{')
+			{
+				start_position = i + 2;
+				break;
+			}
+		}
+
+		for (int i = file_size - 1; i >= 0; --i)
+		{
+
+
+			if (file_buffer[i] == '}')
+			{
+				end_postiion = i;
+				break;
+			}
+		}
+
+		int Index = 0;
+
+		int size;
+		for (int i = start_position; i < end_postiion; ++i)
+		{
+
+
+			if (file_buffer[i] == ':')
+			{
+				for (int j = i + 1; ; ++j)
+				{
+					if (file_buffer[j] == 0x0d)
+					{
+						size = j - i - 1;
+						memcpy_s(ThreadData[Index], size, file_buffer + i + 1, size);
+						ThreadData[Index][size] = '\0';
+						Index++;
+						break;
+					}
+				}
+				start_position = i + size;
+			}
+		}
+
+		free(file_buffer);
+
+		fclose(worker_information);
+	}
+
+
+	return true;
+
+}
