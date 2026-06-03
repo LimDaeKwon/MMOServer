d292c572bdd3c251c1db68169f90a046c1cbb426
DK <gritdrake@gmail.com>
2026-06-03 22:27:15 +0900

    #8 메인 코드 스타일 정리

--- Servers/SelectMMOTCPFighter/MMOTCPFighterServer.cpp
+++ Servers/SelectMMOTCPFighter/MMOTCPFighterServer.cpp
@@ -1,38 +1,30 @@
-﻿// TCPFighterServer.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <iostream>
-#include "Network.h"
-#include "Contents.h"
-#include "ws2tcpip.h"
+﻿#include "Contents.h"
 #include "CrashDump.h"
-#pragma comment(lib,"Winmm.lib")
+#include "Network.h"
 
-CrashDump a;
+#pragma comment(lib, "Winmm.lib")
+
+CrashDump crashDump;
 
 unsigned int GlobalChecksum;
 
-//bool Shutdown = false;
-
-
-
 int main()
 {
-	timeBeginPeriod(1);
-	Initialize();
-	while (1)
-	{
-		Network();
+    timeBeginPeriod(1);
 
-		Update();
+    Initialize();
 
-		//ServerControl();
+    while (true)
+    {
+        Network();
 
-	}
+        Update();
 
-	WSACleanup();
-	timeEndPeriod(1);
-	return 0;
+        // ServerControl();
+    }
 
+    WSACleanup();
+    timeEndPeriod(1);
 
+    return 0;
 }
